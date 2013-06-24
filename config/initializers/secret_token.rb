0bb6adab6ff540f0d62186c62d560107383b36e3
khoa <averageazn1337@gmail.com>
2013-06-23 20:46:21 -0500

    v1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tamulol::Application.config.secret_token = 'aa49528006b7d855b2188b4f3fdb71594969dc052d82754fb040acf341456e6c628cfc90df81524b8b70733f90ae1ad458f6558cbcd1343088bfcbbaae6217a8'
