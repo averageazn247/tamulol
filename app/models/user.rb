dc4c7f80c8ca99499ce50b0328b4e8d5421bc643
averageazn247 <averageazn1337@gmail.com>
2013-06-28 17:16:46 -0500

    sd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
  
-     attr_accessible :name, :email, :password, :password_confirmation,  :phone 
+     attr_accessible :name, :email, :password, :password_confirmation,  :phone , :team_id
   has_secure_password
   has_many :teams
   before_save { |user| user.email = email.downcase }
