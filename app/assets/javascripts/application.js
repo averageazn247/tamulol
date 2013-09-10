7d31686d98b44606a2790ca62c7420b4ac0392ba
khoa <averageazn1337@gmail.com>
2013-09-10 14:04:35 -0500

    sorted

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,16 +10,16 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap
+
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.datepicker
 //= require jquery-ui
-
+//= require bootstrap
 //= require jquery.purr
 //= require_tree .
 //= require best_in_place
-//= require kalendae
+
 
 
 $(document).ready(function() {
