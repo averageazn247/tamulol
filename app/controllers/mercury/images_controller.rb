80b927e3df7a2bc9cbbc6f93d20c647d60048dd0
khoa <averageazn1337@gmail.com>
2014-03-04 18:32:35 -0600

    fix css on home

--- app/controllers/mercury/images_controller.rb
+++ app/controllers/mercury/images_controller.rb
@@ -2,14 +2,12 @@ class Mercury::ImagesController < MercuryController
 
   respond_to :json
 
-  # POST /images.jsonrespond_to :json
-
+  # POST /images.json
   def create
-     @blog_image = BlogImage.new(params[:image])
-     @blog_image.save!
-     render :json => @blog_image.to_json(:only => :image)
+    @image = Mercury::Image.new(params[:image])
+    @image.save
+    respond_with @image
   end
-
 
   # DELETE /images/1.json
   def destroy
