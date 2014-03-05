667bfae4a89b1b93dc19369909d3d3941c8c15a2
khoa <averageazn1337@gmail.com>
2014-03-04 18:25:00 -0600

    fix css on home

--- app/models/blog_image.rb
+++ app/models/blog_image.rb
@@ -1,3 +1,4 @@
 class BlogImage < ActiveRecord::Base
   # attr_accessible :title, :body
+  mount_uploader :image, ImageUploader
 end
--- app/models/mercury/image.rb
+++ app/models/mercury/image.rb
@@ -5,9 +5,14 @@ class Mercury::Image < ActiveRecord::Base
   attr_accessible :image
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-        :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-        :url => "/system/:attachment/:id/:style/:filename"
-
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => "tamulol2013",
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+                    :path => ":attachment/:id/:style/:filename",
+                    :url => ":attachment/:id/:style/:filename"}
+                    
   delegate :url, :to => :image
 
   def serializable_hash(options = nil)
