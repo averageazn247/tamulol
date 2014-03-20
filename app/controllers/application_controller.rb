2f0ccf6493007c4c48584d1601551288abd20ec9
khoa <averageazn1337@gmail.com>
2014-03-20 09:44:26 -0500

    final edit streams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
    include SessionsHelper
-<<<<<<< HEAD
-   
+ 
    def all_streams
     @all_streams=Stream.all 
    end
-=======
-   @streams=Stream.all
->>>>>>> 7720d1421f3f2fc93865a0336fde8ae030b8eb89
+ 
    
    def current_user
     @current_user ||= User.find_by_auth_token( cookies[:auth_token]) if cookies[:auth_token]
