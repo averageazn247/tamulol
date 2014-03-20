b80a99cc97b9901d776b7e07ad2513229a8fcdc3
khoa <averageazn1337@gmail.com>
2014-03-20 09:39:56 -0500

    final edit streams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
    include SessionsHelper
-   @streams=Stream.all
+   
+   def all_streams
+    @all_streams=Stream.all 
+   end
    
    def current_user
     @current_user ||= User.find_by_auth_token( cookies[:auth_token]) if cookies[:auth_token]
   end
+  helper_method :all_streams
   helper_method :current_user
   # Force signout to prevent CSRF attacks
   def handle_unverified_request
